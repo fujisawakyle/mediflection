5749e7437f570044b722f61902a7ba99b1f865c9
fujisawakyle <fujisawakyle@gmail.com>
2017-10-23 22:07:21 -0700

    add media queries

--- client/src/layout.js
+++ client/src/layout.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
+import media from './theme/media';
 
 export const Container = styled.div`
   background: rgba(255, 255, 255, 0.1);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  ${media.md`
+    color:red;
+  `};
 `;
